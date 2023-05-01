712811909d95af0571efe523897ebfc364d5cb58
Michelle Arquiza <marquiza@cs.washington.edu>
2023-05-01 08:33:34 -0700

    input buddy/shop frags

--- src/app/src/main/java/com/example/workoutholicapp/ui/buddy/BuddyFragment.java
+++ src/app/src/main/java/com/example/workoutholicapp/ui/buddy/BuddyFragment.java
@@ -1,4 +1,4 @@
-package com.example.workoutholicapp.ui.statistics;
+package com.example.workoutholicapp.ui.buddy;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,22 +10,22 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.workoutholicapp.databinding.FragmentStatisticsBinding;
+import com.example.workoutholicapp.databinding.FragmentBuddyBinding;
 
-public class StatisticsFragment extends Fragment {
+public class BuddyFragment extends Fragment {
 
-    private FragmentStatisticsBinding binding;
+    private FragmentBuddyBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        StatisticsViewModel statisticsViewModel =
-                new ViewModelProvider(this).get(StatisticsViewModel.class);
+        BuddyViewModel buddyViewModel =
+                new ViewModelProvider(this).get(BuddyViewModel.class);
 
-        binding = FragmentStatisticsBinding.inflate(inflater, container, false);
+        binding = FragmentBuddyBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textStatistics;
-        statisticsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        final TextView textView = binding.textBuddy;
+        buddyViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
--- src/app/src/main/java/com/example/workoutholicapp/ui/shop/ShopViewModel.java
+++ src/app/src/main/java/com/example/workoutholicapp/ui/shop/ShopViewModel.java
@@ -1,16 +1,16 @@
-package com.example.workoutholicapp.ui.statistics;
+package com.example.workoutholicapp.ui.shop;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class StatisticsViewModel extends ViewModel {
+public class ShopViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public StatisticsViewModel() {
+    public ShopViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is the statistics fragment I made");
+        mText.setValue("This is the shop fragment I made");
     }
 
     public LiveData<String> getText() {
