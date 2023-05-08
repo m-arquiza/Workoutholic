035ba30ce972a6594d5acdc0efc0874da27aea68
Michelle Arquiza <marquiza@cs.washington.edu>
2023-05-08 06:59:26 -0700

    the ball appears

--- app/src/main/java/com/example/workoutholicapp/ui/shop/ShopViewModel.java
+++ app/src/main/java/com/example/workoutholicapp/ui/shop/ShopViewModel.java
@@ -8,6 +8,23 @@ import androidx.lifecycle.ViewModel;
 
 
 public class ShopViewModel extends ViewModel {
+        /* hi ashley! some to dos that I can think of rn:
+            - currently, food and water bought is stored as a variable in the shopviewmodel class.
+              i'd like to be able to use it in the buddy class as well, but i'm a) not able to use
+              shop vars in buddy and b) not able to call buddy component ids in shop. if you could
+              fix that that'd be perf :)
+            - similarly (but also not rly) when i have a toy pressed, nothing happens... i hardcoded
+              a single toy in, but since there's no way for me to indicate that the user alr bought
+              an item in the shop to use in the buddy fragment i didn't create any variables so same idea,
+              if you can give me a way to access that information that helps
+            - vitals do not move in any way. atm i have vital bars set to 80dp height, if you can
+              provide me numbers such that i can change the height of the vitals according to those
+              80 degrees of measurement i would be very grateful
+            - so,,, shop system not implemented. not sure who's doing that, but i can't have items
+              cost things if i have no money to spend LOL so money doesn't go up or down, legit
+              just hardcoded as like 104 or smth
+           and that's all i can think of rn
+        * */
 
     private MutableLiveData<Integer> foodInStorage = new MutableLiveData<>(0);
     private MutableLiveData<Integer> waterInStorage = new MutableLiveData<>(0);
