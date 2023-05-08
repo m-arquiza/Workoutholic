a12b381ce509f643876ef842ed9d430cc1128e40
Sonnhid <sonnhid@uw.edu>
2023-05-08 05:09:38 -0700

    workoutplan bug fix

--- app/src/main/java/com/example/workoutholicapp/backend/WorkoutPlans/WorkoutPlans.java
+++ app/src/main/java/com/example/workoutholicapp/backend/WorkoutPlans/WorkoutPlans.java
@@ -5,15 +5,17 @@ import static com.example.workoutholicapp.backend.ViewWorkout.ViewWorkout.View;
 
 import com.example.workoutholicapp.backend.ViewWorkout.Exercise;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 public class WorkoutPlans {
-    private static List<WorkoutPlan> workoutplans;
+    private static List<WorkoutPlan> workoutplans = new ArrayList<>();
     public static void main(String[] args){
         List<Exercise> list = JSONmapper(View("chest"));
         Create(list,"chest workouts");
         List<Exercise> test = Choose("chest workouts");
+        System.out.println(test.size());
         Iterator<Exercise> itr = test.iterator();
         while(itr.hasNext()){
             System.out.println(itr.next().getName());
@@ -29,8 +31,9 @@ public class WorkoutPlans {
         Iterator<WorkoutPlan> itr = workoutplans.iterator();
         List<Exercise> output =null;
         while(itr.hasNext()){
-            if(itr.next().getName().equals(input)){
-                output = itr.next().getList();
+            WorkoutPlan next = itr.next();
+            if(next.getName().equals(input)){
+                output = next.getList();
             }
         }
         return output;
