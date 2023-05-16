35888cc7625b0696b607960e660590322d40c148
Michelle Arquiza <marquiza@cs.washington.edu>
2023-05-16 10:43:33 -0700

    fixed cross-frag vars by splitting into multiple onlicks per frag

--- app/src/main/java/com/example/workoutholicapp/ui/MainViewModel.java
+++ app/src/main/java/com/example/workoutholicapp/ui/MainViewModel.java
@@ -11,28 +11,35 @@ public class MainViewModel extends ViewModel {
     private MutableLiveData<Integer> waterStorage = new MutableLiveData<>(0);
     private MutableLiveData<Integer> totalMoney = new MutableLiveData<>(150);
 
-    public void onFoodClick(boolean isShop) {
+    public void shopFoodClick() {
         Integer food = foodStorage.getValue();
         Integer money = totalMoney.getValue();
-        if (isShop && money >= 20) {
-            foodStorage.setValue(food+1);
+        if (money >= 20) {
+            foodStorage.setValue(food + 1);
             totalMoney.setValue(money - 20);
-        } else if (food != null && food != 0) {
+        }
+    }
+    public void buddyFoodClick() {
+        Integer food = foodStorage.getValue();
+        if (food != 0) {
             foodStorage.setValue(food-1);
         }
     }
 
-    public void onWaterClick(boolean isShop) {
+    public void shopWaterClick() {
         Integer water = waterStorage.getValue();
         Integer money = totalMoney.getValue();
-        if (isShop && money >= 10) {
-            waterStorage.setValue(water+1);
+        if (money >= 10) {
+            waterStorage.setValue(water + 1);
             totalMoney.setValue(money - 10);
-        } else if (water != null && water != 0) {
+        }
+    }
+    public void buddyWaterClick() {
+        Integer water = waterStorage.getValue();
+        if (water != 0) {
             waterStorage.setValue(water-1);
         }
     }
-
     public LiveData<Integer> foodCount() {
         return foodStorage;
     }
@@ -45,3 +52,4 @@ public class MainViewModel extends ViewModel {
         return totalMoney;
     }
 }
+
