071be54168b800c648318141aff18e96043e6781
Michelle Arquiza <marquiza@cs.washington.edu>
2023-05-16 19:06:20 -0700

    fixed up test dependencies + working frontend tests (incomplete)

--- app/src/test/java/com/example/workoutholicapp/BasicShopTests.java
+++ app/src/test/java/com/example/workoutholicapp/BasicShopTests.java
@@ -2,28 +2,65 @@ package com.example.workoutholicapp;
 
 import static org.junit.Assert.assertEquals;
 
-import com.example.workoutholicapp.ui.shop.ShopViewModel;
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
 
+import com.example.workoutholicapp.ui.MainViewModel;
+
+import org.junit.Rule;
 import org.junit.Test;
 
 
 public class BasicShopTests {
 
+    @Rule
+    public InstantTaskExecutorRule instantTaskExecutorRule = new InstantTaskExecutorRule();
     @Test
-    public void testFoodClick() {
-        ShopViewModel vm = new ShopViewModel();
-        vm.onFoodClick();
-        vm.onFoodClick();
-        vm.onFoodClick();
-        assertEquals(3, vm.foodCount());
+    public void testNoFoodWater() {
+        MainViewModel vm = new MainViewModel();
+        int food = vm.foodCount().getValue();
+        int water = vm.waterCount().getValue();
+        assertEquals(0, food);
+        assertEquals(0, water);
     }
 
     @Test
-    public void testWaterClick() {
-        ShopViewModel vm = new ShopViewModel();
-        vm.onFoodClick();
-        vm.onFoodClick();
-        vm.onFoodClick();
-        assertEquals(3, vm.waterCount());
+    public void testFoodClickEnoughMoney() {
+        MainViewModel vm = new MainViewModel();
+        for (int i = 0; i < 3; i++) {
+            vm.shopFoodClick();
+        }
+        int food = vm.foodCount().getValue();
+        assertEquals(3, food);
     }
+
+    @Test
+    public void testWaterClickEnoughMoney() {
+        MainViewModel vm = new MainViewModel();
+        for (int i = 0; i < 7; i++) {
+            vm.shopWaterClick();
+        }
+        int water = vm.waterCount().getValue();
+        assertEquals(7, water);
+    }
+
+    @Test
+    public void testWaterClickNotEnoughMoney() {
+        MainViewModel vm = new MainViewModel();
+        for (int i = 0; i < 20; i++) {
+            vm.shopWaterClick();
+        }
+        int water = vm.waterCount().getValue();
+        assertEquals(15, water);
+    }
+
+    @Test
+    public void testFoodClickNotEnoughMoney() {
+        MainViewModel vm = new MainViewModel();
+        for (int i = 0; i < 8; i++) {
+            vm.shopFoodClick();
+        }
+        int food = vm.foodCount().getValue();
+        assertEquals(7, food);
+    }
+
 }
